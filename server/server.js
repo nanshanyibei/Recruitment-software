0b93ac33e52e926be1db6823e1ae9b66a07c7804
[nanshanyibei] <[1641685360@qq.com]>
2019-07-06 22:25:07 +0800

    test

--- server/server.js
+++ server/server.js
@@ -13,13 +13,36 @@ const User = mongoose.model('user', new mongoose.Schema({
 	age: {type: Number, require: true}
 }))
 
+// 新增数据
+User.create({
+	user: 'xiaoming',
+	age: 20
+}, function(err, doc) {
+	if (!err) {
+		console.log(doc)
+	} else {
+		console.log(err)
+	}
+})
+
+// User.remove({
+// 	age: 18
+// }, function(err, doc) {
+// 	console.log(doc)
+// })
+
+// User.update({"user": "xiaoming"},{'$set': {age: 26}}, function(err,doc) {
+// 	console.log(doc)
+// })
 //新建APP
 const app = express();
 app.get("/",function(req,res){
 	res.send("<h1>Hello World</h1>")
 })
 app.get('/data',function(req,res){
-	res.json({name:'imooc TEST',type:'IT'})
+	User.findOne({user: 'xiaoming'}, function(err, doc){
+		res.json(doc)
+	})
 })
 app.listen(9093,function(){
   console.log("Node app start at port 9093")
