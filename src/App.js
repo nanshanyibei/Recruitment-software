024aaf436e2f7b1956d5b08157286bd745f011d7
[nanshanyibei] <[1641685360@qq.com]>
2019-07-07 20:15:33 +0800

    用装饰器写connect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addGUN, removeGUN, addGunAsync} from './index.redux'
 
+@connect(
+	//要state什么属性放到props里
+	state => ({ num: state }),
+	//要用什么方法，放到props，自动dispatch
+	{ addGUN, removeGUN, addGunAsync }
+)
+
 class App extends React.Component{
 	render(){
 		return <div>
@@ -12,12 +19,5 @@ class App extends React.Component{
 		</div>
 	}
 }
-const mapStatetoProps = (state) => {
-	return { num: state }
-}
-
-const actionCreator = { addGUN, removeGUN, addGunAsync }
-
-App = connect(mapStatetoProps, actionCreator)(App)
 
 export default App
