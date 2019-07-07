0e8a85e5a570eff52cc970bf219b18c2dadf0617
[nanshanyibei] <[1641685360@qq.com]>
2019-07-07 09:55:45 +0800

    React添加

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Button} from 'antd-mobile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React + Redux
-        </a>
-      </header>
+class App extends React.Component{
+  render(){
+    const boss = '李云东'
+    return <div>
+      <h2>独立团，团长：{boss}</h2>
+      <FristCamp campBoss = '张大喵' />
+      <Cavalry CavalryBoss = '孙德胜' />
     </div>
-  );
+  }
 }
 
-export default App;
+function Cavalry(props) {
+  return <h2>骑兵连连长，{props.CavalryBoss}，冲啊</h2>
+}
+
+class FristCamp extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      solders: ['虎子','柱子','王根生']
+    }
+  }
+  addSolder = () => {
+    this.setState({
+      solders: [...this.state.solders,'新兵蛋子'+Math.random()]
+    })
+  }
+  render(){
+    return <div>
+      <h2>一营营长：{this.props.campBoss}</h2>
+      <Button type='primary' onClick={this.addSolder}>新兵入伍</Button>
+      <ul>
+        {this.state.solders.map(v => {
+          return <li key={v}>{v}</li>
+        })}
+      </ul>
+    </div>
+  }
+}
+
+export default App
