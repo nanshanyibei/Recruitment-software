5fc6985c9fa915a236a7dd8999787c228c917a53
[nanshanyibei] <[1641685360@qq.com]>
2019-07-07 11:32:53 +0800

    store，感觉还是不是很懂

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import React from 'react'
-import {Button} from 'antd-mobile'
+import { addGUN } from './index.redux'
 
 class App extends React.Component{
-  render(){
-    const boss = '李云东'
-    return <div>
-      <h2>独立团，团长：{boss}</h2>
-      <FristCamp campBoss = '张大喵' />
-      <Cavalry CavalryBoss = '孙德胜' />
-    </div>
-  }
-}
-
-function Cavalry(props) {
-  return <h2>骑兵连连长，{props.CavalryBoss}，冲啊</h2>
-}
-
-class FristCamp extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      solders: ['虎子','柱子','王根生']
-    }
-  }
-  addSolder = () => {
-    this.setState({
-      solders: [...this.state.solders,'新兵蛋子'+Math.random()]
-    })
-  }
-  render(){
-    return <div>
-      <h2>一营营长：{this.props.campBoss}</h2>
-      <Button type='primary' onClick={this.addSolder}>新兵入伍</Button>
-      <ul>
-        {this.state.solders.map(v => {
-          return <li key={v}>{v}</li>
-        })}
-      </ul>
-    </div>
-  }
+  // constructor(props){
+	// 	super(props)
+	// 	this.state = {
+	// 		test: "1234"
+	// 	}
+	// }
+	render(){
+		const store = this.props.store
+		const num = store.getState()
+		return <div>
+			<h1>现在有{num}</h1>
+			<button onClick = {() => store.dispatch(addGUN())}>再多一个</button>
+		</div>
+	}
 }
 
 export default App
