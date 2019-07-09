b1c0e8c4db784fcdb9a1e67e2eb2a28f9fbead84
[nanshanyibei] <[1641685360@qq.com]>
2019-07-09 22:22:54 +0800

    Login和Register联调完成

--- server/user.js
+++ server/user.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const utils = require('utility')
 const Router = express.Router()
 const model = require('./model')
 const User = model.getModel("user")
 
 Router.get('/list',function(req, res){
+	//User.remove({},function(err, doc){})
 	User.find({}, function(err, doc){
 		return res.json(doc)
+	})
+})
+
+
+Router.post('/login', function(req, res){
+	const {user, pwd} = req.body
+	User.findOne({user, pwd: md5Pwd(pwd)}, {pwd: 0}, function(err,doc){
+		if(!doc){
+			return res.json({code: 1, msg: "用户名或密码输入错误"})
+		}
+		return res.json({code: 0, data: doc})
 	})
 })
 
@@ -16,7 +29,7 @@ Router.post('/register', function(req, res){
 		if(doc){
 			return res.json({code: 1, msg: "用户名重复"})
 		}
-		User.create({user, pwd, type}, function(err, doc){
+		User.create({user, type, pwd: md5Pwd(pwd)}, function(err, doc){
 			if(err) {
 				return res.json({code: 1, msg: "后端出错了"})
 			}
@@ -29,5 +42,10 @@ Router.get('/info', function(req,res){
 	//用户有没有cookie
 	return res.json({code: 1})
 })
+
+function md5Pwd(pwd){
+	const salt = 'imooc_is_good_3957x8yza6!@#IUHJh~~'
+	return utils.md5(utils.md5(pwd + salt))
+}
 
 module.exports = Router
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { getRedirectPath } from '../util'
 
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 const ERROR_MSG = 'ERROR_MSG'
+const LOGIN_SUCESS = 'LOGIN_SUCESS'
 
 const initState = {
 	isAuth: "",
@@ -15,7 +17,9 @@ const initState = {
 export function user(state = initState, action){
 	switch(action.type){
 		case REGISTER_SUCCESS:
-			return {...state, msg: "", isAuth: true, ...action.payload}
+			return {...state, msg: "", redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
+		case LOGIN_SUCESS:
+			return {...state, msg: "", redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
 		case ERROR_MSG:
 			return {...state, msg: action.msg, isAuth: false}
 		default:
@@ -27,8 +31,28 @@ function errorMsg(msg){
 	return {msg, type: ERROR_MSG}
 }
 
+function loginSuccess(data){
+	return {type: LOGIN_SUCESS, payload: data}
+}
+
 function registerSuccess(data){
 	return {type: REGISTER_SUCCESS, payload: data}
+}
+
+export function login({user, pwd}){
+	if(!user || !pwd){
+		return errorMsg("用户密码必须输入")
+	}
+	return dispatch => {
+		axios.post('/user/login', {user, pwd})
+			.then(res => {
+				if(res.status === 200 && res.data.code === 0){
+					dispatch(loginSuccess(res.data.data))
+				} else {
+					dispatch(errorMsg(res.data.msg))
+				}
+			})
+	}
 }
 
 export function register({user, pwd, repeatpwd, type}){
